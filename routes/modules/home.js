4bb9709bf15baa20fe20ab036ae72174f3dc73ad
william1993314 <william1993314@gmail.com>
2021-09-03 18:17:06 -0400

    Add Total expense function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,9 +5,15 @@ const router = express.Router()
 const Record = require('../../models/Record')
 // 定義首頁路由
 router.get('/', (req, res) => {
+    let totalAmount = 0
     Record.find()
         .lean()
-        .then(records => res.render('index', { records }))
+        .then(records => {
+            records.forEach(record => {
+                totalAmount += record.amount
+            })
+            res.render('index', { records, totalAmount})
+})
         .catch(error => console.error(error))
 })
 // 匯出路由模組
