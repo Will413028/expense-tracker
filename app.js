c2fe4ba90215a35bb768c6f2efba15f9e6e69e34
william1993314 <william1993314@gmail.com>
2021-09-03 19:31:57 -0400

    Heroku init

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const PORT = process.env.PORT || 3000
 
 require('./config/mongoose')
 
@@ -21,6 +22,6 @@ app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
 
-app.listen(3000, () => {
-    console.log('App is running on http://localhost:3000.')
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
 })
