f8168a4296be9103693af8c0a213c2b2ed0032da
william1993314 <william1993314@gmail.com>
2021-09-03 20:31:50 -0400

    Add records.json for recordSeeder.js

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,37 +1,8 @@
-const Record = require('../record')
 const db = require('../../config/mongoose')
+const Record = require('../Record')
 
-const recordList = [
-    {
-    name: "AMC",
-    date: new Date("2021-08-12"),
-    amount: 20,
-    category: "Leisure & Entertainment"
-    },
-    {
-        name: "Burger King",
-        date: new Date("2021-08-12"),
-        amount: 15,
-        category: "Restaurant & Dining"
-    },
-    {
-        name: "Metro",
-        date: new Date("2021-08-11"),
-        amount: 120,
-        category: "Transportation"
-    },
-    {
-        name: "Traffic ticket",
-        date: new Date("2021-09-01"),
-        amount: 220,
-        category: "Other"
-    },
-    {
-        name: "Tello",
-        date: new Date("2021-09-01"),
-        amount: 10,
-        category: "Home & Utilities"
-    }]
+const recordList = require('../../records.json')
+console.log(recordList.results)
 
 
 
