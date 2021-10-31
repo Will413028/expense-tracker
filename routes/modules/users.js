6df3cf39d57718bad121cfef5ad5a362d352aba1
william1993314 <william1993314@gmail.com>
2021-10-31 20:47:04 +0800

    Add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -12,17 +12,29 @@ router.post('/login', passport.authenticate('local', {
     failureRedirect: 'users/login'
 }))
 
-router.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/users/login')
-})
-
 router.get('/register', (req, res) => {
     res.render('register')
 })
 
 router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
+
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
+        return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+        })
+    }
 
     User.findOne({ email }).then(user => {
         if (user) {
@@ -43,6 +55,12 @@ router.post('/register', (req, res) => {
                 .catch(err => console.log(err))
         }
     })
+})
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    req.flash('success_msg', '你已經成功登出。')
+    res.redirect('/users/login')
 })
 
 module.exports = router
