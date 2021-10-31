fc0e60e9cd054a070d4f26b54c36ba24dd4c5cd2
william1993314 <william1993314@gmail.com>
2021-10-31 15:14:24 +0800

    Add user modules

---
+++ helper/handlebars.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    eq: function (a, b) {
+        if (a === b) {
+            return true
+        } else {
+            return false
+        }
+    },
+    total: function (array) {
+        let sum = 0
+
+        array.forEach(function (doc) {
+            sum += doc.amount
+        })
+
+        return sum // return total
+    },
+    dateFormate: function (date) {
+        return date.toJSON().substring(0, 10)
+    },
+}
